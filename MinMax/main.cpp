720cc16c613fdcd455c9a93a1e37c97023cc8abd
Jshewmaker <jshewmak@gmail.com>
2017-09-25 13:34:14 -0400

    Initial Commit

---
+++ MinMax/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MinMax
+//
+//  Created by Joshua Shewmaker on 9/25/17.
+//  Copyright © 2017 Joshua Shewmaker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
